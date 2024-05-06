0178bb74f702f8f81dbf37e8c103d40603fb4cd7
DevKike <alvaronarvaez151@gmail.com>
2024-05-06 02:09:33 -0500

    Worked on methods for findBy validations

--- src/main/java/com/onclass/user/microservice/adapters/driven/jpa/mysql/repository/IUserRepository.java
+++ src/main/java/com/onclass/user/microservice/adapters/driven/jpa/mysql/repository/IUserRepository.java
@@ -3,6 +3,9 @@ package com.onclass.user.microservice.adapters.driven.jpa.mysql.repository;
 import com.onclass.user.microservice.adapters.driven.jpa.mysql.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepository extends JpaRepository<UserEntity, Long> {
+import java.util.List;
 
+public interface IUserRepository extends JpaRepository<UserEntity, Long> {
+    List<UserEntity> findByDocument(String document);
+    List<UserEntity> findByEmail(String email);
 }
