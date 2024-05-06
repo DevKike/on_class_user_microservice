ce0867196a2d328afb36b34965023a9d30e0ef5a
DevKike <alvaronarvaez151@gmail.com>
2024-05-06 01:02:09 -0500

    Controller advisor for exceptions was added, including not found exception and invalid email exception

---
+++ src/main/java/com/onclass/user/microservice/configuration/exceptionhandler/ControllerAdvisor.java
@@ -0,0 +1,34 @@
+package com.onclass.user.microservice.configuration.exceptionhandler;
+
+import com.onclass.user.microservice.adapters.driven.jpa.mysql.exception.NotFoundException;
+import com.onclass.user.microservice.configuration.Constants;
+import com.onclass.user.microservice.domain.exception.InvalidEmailException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ControllerAdvisor {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleNotFoundException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse(
+                Constants.NOT_FOUNT_EXCEPTION_MESSAGE,
+                HttpStatus.NOT_FOUND.toString(),
+                LocalDateTime.now()
+        ));
+    }
+
+    @ExceptionHandler(InvalidEmailException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidEmailException() {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(
+                Constants.INVALID_EMAIL_EXCEPTION_MESSAGE,
+                HttpStatus.BAD_REQUEST.toString(),
+                LocalDateTime.now()
+        ));
+    }
+}
