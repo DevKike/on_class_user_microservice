348993605570fe678f409a9ff126204dcb216155
DevKike <alvaronarvaez151@gmail.com>
2024-05-06 01:00:35 -0500

    Added constants for validations

---
+++ src/main/java/com/onclass/user/microservice/configuration/Constants.java
@@ -0,0 +1,10 @@
+package com.onclass.user.microservice.configuration;
+
+public class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String NOT_FOUNT_EXCEPTION_MESSAGE = "Recourse not found";
+    public static final String INVALID_EMAIL_EXCEPTION_MESSAGE = "Invalid email address";
+}
