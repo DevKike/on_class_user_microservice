d87215fb16cfc025765f92e42b815d115123f128
DevKike <alvaronarvaez151@gmail.com>
2024-05-06 02:08:47 -0500

    Already exists were added

---
+++ src/main/java/com/onclass/user/microservice/adapters/driven/jpa/mysql/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.onclass.user.microservice.adapters.driven.jpa.mysql.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException() {
+        super();
+    }
+}
