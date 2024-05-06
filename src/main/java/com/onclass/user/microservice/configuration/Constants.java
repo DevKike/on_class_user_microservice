90285878c973df82fc817038751aae6d0a07c427
DevKike <alvaronarvaez151@gmail.com>
2024-05-06 02:08:08 -0500

    Exceptions messages were added

--- src/main/java/com/onclass/user/microservice/configuration/Constants.java
+++ src/main/java/com/onclass/user/microservice/configuration/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
 
     public static final String NOT_FOUNT_EXCEPTION_MESSAGE = "Recourse not found";
     public static final String INVALID_EMAIL_EXCEPTION_MESSAGE = "Invalid email address";
+    public static final String DOCUMENT_ALREADY_EXISTS_EXCEPTION_MESSAGE = "Document already exists";
+    public static final String EMAIL_ALREADY_EXISTS_EXCEPTION_MESSAGE = "Email already exists";
 }
